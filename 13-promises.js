3701ba73ea7efc573cc0c9332776b88c927c8366
Peter Oehman <tucker2923@gmail.com>
2022-01-08 12:52:23 -0500

    promises with readfile and writefile

---
+++ 13-promises.js
@@ -0,0 +1,29 @@
+const {readFile, writeFile} = require('fs').promises
+
+const start = async() => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile('./content/result-promise.txt', `${first}, ${second}`)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
+
+// getText('./content/first.txt')
+//   .then(result => console.log(result))
+//   .catch((err) => console.log(err))
